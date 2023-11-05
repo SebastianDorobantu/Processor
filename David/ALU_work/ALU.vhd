2a7722173081a2b57fcff9fd83cc34a3aa347b4b
dvgrmm <grimmdavid6@gmail.com>
2023-11-05 20:38:02 +0100

    damn

--- David/ALU_work/ALU.vhd
+++ David/ALU_work/ALU.vhd
@@ -12,193 +12,53 @@ port(	reset : in std_logic;
 
 		--zero_condition : out std_logic;					-- checks if result is 0				
 		--neg_flag : out std_logic;						-- result is negative
-		ALU_out : out std_logic_vector (15 downto 0);				-- output of ALU
-		ovflow_flag : out std_logic;						-- overflow flag
-		proc_complete : out std_logic						-- process complete => notifies that data is placed on the bus
+		ALU_out : out std_logic_vector (15 downto 0):= (others => '0');				-- output of ALU
+		ovflow_flag : out std_logic := '0';						-- overflow flag
+		proc_complete : out std_logic := '0';						-- process complete => notifies that data is placed on the bus
+		overflow_out : out std_logic_vector(15 downto 0):= (others => '0')
 		);	
 end ALU;
 
 architecture bhv of ALU is
-signal ALU_sum : std_logic_vector (15 downto 0);
-signal pos_ovflow : std_logic;
-signal neg_ovflow : std_logic;
-signal overflow : std_logic;
+signal ALU_sum : std_logic_vector (15 downto 0) := (others => '0');
+signal pos_ovflow : std_logic := '0';
+signal neg_ovflow : std_logic := '0';
+signal overflow : std_logic := '0';
+signal ALU_A_lshift : std_logic_vector (15 downto 0) := (others => '0');
+signal ALU_B_lshift : std_logic_vector (15 downto 0) := (others => '0');
+signal lshift_output : std_logic_vector (15 downto 0) := (others => '0');
+signal ovflow_flag_lshift : std_logic := '0';
+signal overflow_out_lshift : std_logic_vector (15 downto 0) := (others => '0');
 
-component lshifter is
-port(
-input  : IN  std_logic_vector (14 DOWNTO 0);
-shift1 : out std_logic_vector (14 DOWNTO 0);
-shift2 : out std_logic_vector (14 DOWNTO 0);
-shift3 : out std_logic_vector (14 DOWNTO 0);
-shift4 : out std_logic_vector (14 DOWNTO 0);
-shift5 : out std_logic_vector (14 DOWNTO 0);
-shift6 : out std_logic_vector (14 DOWNTO 0);
-shift7 : out std_logic_vector (14 DOWNTO 0);
-shift8 : out std_logic_vector (14 DOWNTO 0);
-shift9 : out std_logic_vector (14 DOWNTO 0);
-shift10 : out std_logic_vector (14 DOWNTO 0);
-shift11 : out std_logic_vector (14 DOWNTO 0);
-shift12 : out std_logic_vector (14 DOWNTO 0);
-shift13 : out std_logic_vector (14 DOWNTO 0);
-shift14 : out std_logic_vector (14 DOWNTO 0);
-shift15 : out std_logic_vector (14 DOWNTO 0);
-shift16 : out std_logic_vector (14 DOWNTO 0);
-output : out std_logic_vector (14 downto 0);
-overflow_vector : out std_logic_vector(14 DOWNTO 0 );
-overflow_out : out std_logic_vector(14 DOWNTO 0 );
-signbit : out std_logic
-);
-end component;
 
 component hl_lshifter is
 port(
-ALU_A2 : in std_logic_vector(15 downto 0);
-ALU_B2 : in std_logic_vector(15 downto 0)
+ALU_A : in std_logic_vector(15 downto 0);
+ALU_B : in std_logic_vector(15 downto 0);
+output : out std_logic_vector(15 downto 0):= (others => '0');
+overflow_vec : out std_logic_vector(15 downto 0):= (others => '0');
+ovflow_flag	: out std_logic
 );
 end component;
 
 
 begin
 
-highlevel: hl_lshifter
+port_ALU: hl_lshifter
 PORT MAP(
-input <= ALU_A2 (14 downto 0);
-signbit <= ALU_A2(15);
-nbit_shift <= to_integer(unsgined(ALU_B2(15 downto 0)))
+ALU_A => ALU_A_lshift,
+ALU_B => ALU_B_lshift,
+output => lshift_output,
+overflow_vec => overflow_out_lshift,
+ovflow_flag => ovflow_flag_lshift
 );
+ 		
+ALU_A_lshift <= ALU_A when ALU_cntrl = "0010";
+ALU_B_lshift <= ALU_B when ALU_cntrl = "0010";
+ovflow_flag <= ovflow_flag_lshift when ALU_cntrl = "0010";
+overflow_out <= overflow_out_lshift when ALU_cntrl = "0010";
+ALU_out <= lshift_output when ALU_cntrl = "0010";
 
-leftshift: lshifter
-PORT MAP(
-input => ALU_A (14 downto 0);
-signbit <= ALU_A;
-nbit_shift <= to_integer(unsgined(ALU_B2(15 downto 0)))
-);
 
-process(clk,reset)
-variable vec0, vec1, vec2 : std_logic_vector (15 downto 0);
-variable nbits : integer;
-variable n : integer;
-variable vec_length : integer := 15;
-begin
 
-if reset = '0' then
-	ALU_sum <= (others => '0'); 
-	pos_ovflow <= '0';
-	neg_ovflow <= '0';
-	ovflow_flag <= '0';
-	overflow <= '0';
-	vec0 := (others => '0');
-	vec1 := (others => '0');
-	carry_out <= '0';
-	ALU_out <= (others => '0');
-	proc_complete <= '0';
-	--zero_condition  <=  '0';
-	--neg_flag <= '0';
-	
-
-elsif rising_edge(clk) and complete = true then 						-- soft reset the outputs	
-	ALU_sum <= (others => '0');
-	pos_ovflow <= '0';
-	neg_ovflow <= '0';
-	ovflow_flag <= '0';
-	overflow <= '0';
-	vec0 := (others => '0');
-	vec1 := (others => '0');
-	carry_out <= '0';
-	ALU_out <= (others => '0');
-	proc_complete <= '0';
-	--zero_condition  <=  '0';
-	--neg_flag <= '0';
-	
-elsif rising_edge(clk) and complete = false then 
-	
-	if ALU_A(ALU_A'left)= '0' and ALU_B(ALU_B'left)= '0' then		-- Positive overflow chance
-		pos_ovflow <= '1';
-		
-	else																				
-		pos_ovflow <= '0';
-		
-	end if;
-	
-	if ALU_A(ALU_A'left)= '1' and ALU_B(ALU_B'left)= '1' then		-- Negative overflow chance
-		neg_ovflow <= '1';
-		
-	else
-		neg_ovflow <= '0';
-		
-	end if;
-	
-
-		if ALU_A /= "ZZZZZZZZZZZZZZZZ" then										-- check for Valid input 
-		case ALU_cntrl is													-- check OP field
-			when "0000" =>
-			ALU_sum	<= std_logic_vector(signed(ALU_A(15 downto 0)) + signed(ALU_B(15 downto 0)));
-			if (signed(ALU_sum) < 0 and pos_ovflow = '1') or (signed(ALU_sum) > 0 and neg_ovflow = '1') then	-- check for overflow
-				overflow <= '1';
-		
-			else
-				overflow <= '0';
-			
-			end if;	
-				if overflow = '0' then 										-- overflow is checked here
-					ALU_out <= ALU_sum;
-					proc_complete <= '1';
-					
-				elsif overflow = '1' then
-					ALU_out <= (others => 'Z');
-					ovflow_flag <= '1';
-					
-				end if;
-			when "0001" =>
-				
-				if neg_ovflow = '1' then
-					ALU_out <= std_logic_vector(signed(ALU_A) + (not signed(ALU_B))+ 1);
-					proc_complete <= '1';
-				elsif pos_ovflow = '1' then
-					ALU_out <= std_logic_vector(signed(ALU_A) - signed(ALU_B));
-					proc_complete <= '1';
-				elsif (ALU_A(ALU_A'left)= '0' and ALU_B(ALU_B'left)= '1') then
-						ALU_sum <= std_logic_vector(signed(ALU_A) + (not signed(ALU_B))+ 1);
-						ALU_out <= ALU_sum;
-						proc_complete <= '1';
-					if (signed(ALU_sum) < 0) then
-						ALU_out <= (others => 'Z');
-						ovflow_flag <= '1';
-					end if;
-				elsif (ALU_A(ALU_A'left)= '1' and ALU_B(ALU_B'left)= '0') then
-						ALU_sum <= std_logic_vector(signed(ALU_A) - signed(ALU_B));
-						ALU_out <= ALU_sum;
-						proc_complete <= '1';
-					if (signed(ALU_sum) > 0) then
-						ALU_out <= (others => 'Z');
-						ovflow_flag <= '1';
-					end if;
-				end if;
-				if overflow = '1' then
-					ALU_out <= (others => 'Z');
-					ovflow_flag <= '1';
-			
-				end if;
-			when "0010" => 		
-				ALU_A2 <= ALU_A;
-				ALU_B2 <= ALU_B;
-				overflow_vector <=
-				if overflow_vector /= '000000000000000' then	
-					ovflow_flag <= '1';
-					overflow_out <= signbit & overflow_vector;
-					ALU_out <= output & '0';
-				else
-					ALU_out <= signbit & output;
-					proc_complete <= '1';
-				end if;
-			when others =>
-				ALU_out <= (others => 'Z');
-
-		end case;
-		elsif ALU_A = "ZZZZZZZZZZZZZZZZ" then								-- if input A is disconnected (needs to be connected in all types of functions)
-			ALU_out <= (others => 'Z');										-- keep output disconnected  
-		end if;
-		end if;
-		
-end process;
 end bhv;
--- David/ALU_work/lshifter.vhd
+++ David/ALU_work/lshifter.vhd
@@ -1,4 +1,5 @@
 library IEEE;
+library work;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
@@ -6,94 +7,217 @@ use IEEE.NUMERIC_STD.ALL;
 entity lshifter IS 
 
 port(
-input  : IN  std_logic_vector (14 DOWNTO 0);
-shift1 : out std_logic_vector (14 DOWNTO 0);
-shift2 : out std_logic_vector (14 DOWNTO 0);
-shift3 : out std_logic_vector (14 DOWNTO 0);
-shift4 : out std_logic_vector (14 DOWNTO 0);
-shift5 : out std_logic_vector (14 DOWNTO 0);
-shift6 : out std_logic_vector (14 DOWNTO 0);
-shift7 : out std_logic_vector (14 DOWNTO 0);
-shift8 : out std_logic_vector (14 DOWNTO 0);
-shift9 : out std_logic_vector (14 DOWNTO 0);
-shift10 : out std_logic_vector (14 DOWNTO 0);
-shift11 : out std_logic_vector (14 DOWNTO 0);
-shift12 : out std_logic_vector (14 DOWNTO 0);
-shift13 : out std_logic_vector (14 DOWNTO 0);
-shift14 : out std_logic_vector (14 DOWNTO 0);
-shift15 : out std_logic_vector (14 DOWNTO 0);
-shift16 : out std_logic_vector (14 DOWNTO 0);
-overflow_vector : out std_logic_vector(14 DOWNTO 0 );
-)
+input  : IN  std_logic_vector (15 DOWNTO 0);
+shift1 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift2 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift3 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift4 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift5 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift6 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift7 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift8 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift9 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift10 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift11 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift12 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift13 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift14 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+shift15 : out std_logic_vector (14 DOWNTO 0):= (others => '0');
+overflow_vector : out std_logic_vector(14 DOWNTO 0):= "000000000000000";
+overflow_vector1 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector2 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector3 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector4 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector5 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector6 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector7 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector8 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector9 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector10 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector11 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector12 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector13 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector14 : out std_logic_vector(14 DOWNTO 0):= (others => '0');
+overflow_vector15 : out std_logic_vector(14 DOWNTO 0):= (others => '0')
+
+);
 
 end lshifter;
 
 architecture bhv OF lshifter is
-signal signal1 : std_logic_vector (14 downto 0);
-signal signal2 : std_logic_vector (14 downto 0);
-signal signal3 : std_logic_vector (14 downto 0);
-signal signal4 : std_logic_vector (14 downto 0);
-signal signal5 : std_logic_vector (14 downto 0);
-signal signal6 : std_logic_vector (14 downto 0);
-signal signal7 : std_logic_vector (14 downto 0);
-signal signal8 : std_logic_vector (14 downto 0);
-signal signal9 : std_logic_vector (14 downto 0);
-signal signal10 : std_logic_vector (14 downto 0);
-signal signal11 : std_logic_vector (14 downto 0);
-signal signal12 : std_logic_vector (14 downto 0);
-signal signal13 : std_logic_vector (14 downto 0);
-signal signal14 : std_logic_vector (14 downto 0);
-signal signal15 : std_logic_vector (14 downto 0);
-signal signal16 : std_logic_vector (14 downto 0);
+signal input_in : std_logic_vector (15 downto 0):= (others => '0');
+signal signal1 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal2 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal3 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal4 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal5 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal6 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal7 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal8 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal9 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal10 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal11 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal12 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal13 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal14 : std_logic_vector (14 downto 0):= (others => '0');
+signal signal15 : std_logic_vector (14 downto 0):= (others => '0');
+signal overflow_vector_buf_pos: std_logic_vector(14 downto 0) := (others => '0');
+signal overflow_vector_buf_neg: std_logic_vector(14 downto 0) := (others => '1');
+signal overflow_vector1_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector2_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector3_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector4_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector5_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector6_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector7_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector8_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector9_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector10_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector11_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector12_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector13_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector14_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
+signal overflow_vector15_buf :  std_logic_vector(14 DOWNTO 0 ):= (others => '0');
 
 begin
-signal1 <= input(13 DOWNTO 0) & '0';
+input_in <= input;
+
+
+overflow_vector1_buf <= (overflow_vector_buf_neg(14 downto 1)) & input_in(14) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 1)) & input_in(14) when input(15) = '0';
+								
+overflow_vector2_buf <= (overflow_vector_buf_neg(14 downto 2))&overflow_vector1_buf(0)&input_in(13) when input(15) = '1' else
+		  	(overflow_vector_buf_pos(14 downto 2))&overflow_vector1_buf(0)&input_in(13) when input(15) = '0';
+								
+overflow_vector3_buf <= (overflow_vector_buf_neg(14 downto 3))&overflow_vector2_buf(1 downto 0)&input_in(12) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 3))&overflow_vector2_buf(1 downto 0)&input_in(12) when input(15) = '0';
+								
+overflow_vector4_buf <= (overflow_vector_buf_neg(14 downto 4))&overflow_vector3_buf(2 downto 0)&input_in(11) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 4))&overflow_vector3_buf(2 downto 0)&input_in(11) when input(15) = '0';
+								
+overflow_vector5_buf <= (overflow_vector_buf_neg(14 downto 5))&overflow_vector4_buf(3 downto 0)&input_in(10) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 5))&overflow_vector4_buf(3 downto 0)&input_in(10) when input(15) = '0';
+								
+overflow_vector6_buf <= (overflow_vector_buf_neg(14 downto 6))&overflow_vector5_buf(4 downto 0)&input_in(9) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 6))&overflow_vector5_buf(4 downto 0)&input_in(9) when input(15) = '0';
+								
+overflow_vector7_buf <= (overflow_vector_buf_neg(14 downto 7))&overflow_vector6_buf(5 downto 0)&input_in(8) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 7))&overflow_vector6_buf(5 downto 0)&input_in(8) when input(15) = '0';
+								
+overflow_vector8_buf <= (overflow_vector_buf_neg(14 downto 8))&overflow_vector7_buf(6 downto 0)&input_in(7) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 8))&overflow_vector7_buf(6 downto 0)&input_in(7) when input(15) = '0';
+								
+overflow_vector9_buf <= (overflow_vector_buf_neg(14 downto 9))&overflow_vector8_buf(7 downto 0)&input_in(6) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 9))&overflow_vector8_buf(7 downto 0)&input_in(6)  when input(15) = '0';
+							
+overflow_vector10_buf <= (overflow_vector_buf_neg(14 downto 10))&overflow_vector9_buf(8 downto 0)&input_in(5) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 10))&overflow_vector9_buf(8 downto 0)&input_in(5) when input(15) = '0';
+								
+overflow_vector11_buf <= (overflow_vector_buf_neg(14 downto 11))&overflow_vector10_buf(9 downto 0)&input_in(4) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 11))&overflow_vector10_buf(9 downto 0)&input_in(4)  when input(15) = '0';
+								
+overflow_vector12_buf <= (overflow_vector_buf_neg(14 downto 12))&overflow_vector11_buf(10 downto 0)&input_in(3) when input(15) = '1' else
+			(overflow_vector_buf_pos(14 downto 12))&overflow_vector11_buf(10 downto 0)&input_in(3) when input(15) = '0';	
+								
+overflow_vector13_buf <= (overflow_vector_buf_neg(14 downto 13))&overflow_vector12_buf(11 downto 0)&input_in(2) when input(15) = '1' else
+			overflow_vector_buf_pos(14 downto 13)&overflow_vector12_buf(11 downto 0)&input_in(2) when input(15) = '0';
+								
+overflow_vector14_buf <= (overflow_vector_buf_neg(14))&overflow_vector13_buf(12 downto 0)&input_in(1) when input(15) = '1' else
+			overflow_vector_buf_pos(14)&overflow_vector13_buf(12 downto 0)&input_in(1) when input(15) = '0';
+								
+overflow_vector15_buf <=  overflow_vector14_buf(13 downto 0)&input_in(0) when input(15) = '1' else
+			  overflow_vector14_buf(13 downto 0)&input_in(0) when input(15) = '0';
+
+								
+								
+signal1 <=  input_in(13 DOWNTO 0) & '0';
 shift1 <= signal1;
-overflow_vector(0) <= input(14);
+--overflow_vector1_buf(0) <= input_in(14);
+
 signal2 <= signal1(13 DOWNTO 0) & '0';
 shift2 <= signal2;
-overflow_vector(1 downto 0) <= input(14 downto 13); 
+--overflow_vector2_buf(0) <= input_in(13);
+--overflow_vector2_buf(1) <=  overflow_vector1_buf(0); 
+
 signal3 <= signal2(13 DOWNTO 0) & '0';
 shift3 <= signal3;
-overflow_vector(2 downto 0) <= input(14 downto 12);
+--overflow_vector3_buf(0) <=  input_in(12);
+--overflow_vector3_buf(1) <=  overflow_vector2_buf(0);
+--overflow_vector3_buf(2) <=  overflow_vector2_buf(1);
+
 signal4 <= signal3(13 DOWNTO 0) & '0';
 shift4 <= signal4;
-overflow_vector(3 downto 0) <= input(14 downto 11);
+--overflow_vector4_buf(0) <=  input_in(11);
+--overflow_vector4_buf(1) <=  overflow_vector3_buf(0);
+--overflow_vector4_buf(2) <=  overflow_vector3_buf(1);
+--overflow_vector4_buf(3) <=  overflow_vector3_buf(2);
+
 signal5 <= signal4(13 DOWNTO 0) & '0';
 shift5 <= signal5;
-overflow_vector(4 downto 0) <= input(14 downto 10);
+--overflow_vector5_buf(0) <=  input_in(10);
+--overflow_vector5_buf(1) <=  overflow_vector4_buf(0);
+--overflow_vector5_buf(2) <=  overflow_vector4_buf(1);
+--overflow_vector5_buf(3) <=  overflow_vector4_buf(2);
+--overflow_vector5_buf(4) <=  overflow_vector4_buf(3);
 signal6 <= signal5(13 DOWNTO 0) & '0';
 shift6 <= signal6;
-overflow_vector(5 downto 0) <= input(14 downto 9);
+--overflow_vector6_buf(0) <=  input_in(9);
+--overflow_vector6_buf(1) <=  overflow_vector5_buf(0);
+--overflow_vector6_buf(2) <=  overflow_vector5_buf(1);
+--overflow_vector6_buf(3) <=  overflow_vector5_buf(2);
+--overflow_vector6_buf(4) <=  overflow_vector5_buf(3);
+--overflow_vector6_buf(5) <=  overflow_vector5_buf(4);
 signal7 <= signal6(13 DOWNTO 0) & '0';
 shift7 <= signal7;
-overflow_vector(6 downto 0) <= input(14 downto 8);
+--overflow_vector7_buf(0) <=  input_in(8);
+--overflow_vector7_buf(1) <=  overflow_vector6_buf(0);
+--overflow_vector7_buf(2) <=  overflow_vector6_buf(1);
+--overflow_vector7_buf(3) <=  overflow_vector6_buf(2);
+--overflow_vector7_buf(4) <=  overflow_vector6_buf(3);
+--overflow_vector7_buf(5) <=  overflow_vector6_buf(4);
+--overflow_vector7_buf(6) <=  overflow_vector6_buf(5);
 signal8 <= signal7(13 DOWNTO 0) & '0';
 shift8 <= signal8;
-overflow_vector(7 downto 0) <= input(14 downto 7);
+
 signal9 <= signal8(13 DOWNTO 0) & '0';
 shift9 <= signal9;
-overflow_vector(8 downto 0) <= input(14 downto 6);
+
 signal10 <= signal9(13 DOWNTO 0) & '0';
 shift10 <= signal10;
-overflow_vector(9 downto 0) <= input(14 downto 5);
+
 signal11 <= signal10(13 DOWNTO 0) & '0';
 shift11 <= signal11;
-overflow_vector(10 downto 0) <= input(14 downto 4);
+
 signal12 <= signal11(13 DOWNTO 0) & '0';
 shift12 <= signal12;
-overflow_vector(11 downto 0) <= input(14 downto 3);
+
 signal13 <= signal12(13 DOWNTO 0) & '0';
 shift13 <= signal13;
-overflow_vector(12 downto 0) <= input(14 downto 2);
+
 signal14 <= signal13(13 DOWNTO 0) & '0';
 shift14 <= signal14;
-overflow_vector(13 downto 0) <= input(14 downto 1);
+
 signal15 <= signal14(13 DOWNTO 0) & '0';
 shift15 <= signal15;
-overflow_vector(14 downto 0) <= input(14 downto 0);
---signal16 <= signal15(13 DOWNTO 0) & '0';
---shift16 <= signal16;
+
+
+
+
+overflow_vector1 <= overflow_vector1_buf ;
+overflow_vector2 <= overflow_vector2_buf;
+overflow_vector3 <= overflow_vector3_buf;
+overflow_vector4 <= overflow_vector4_buf;
+overflow_vector5 <= overflow_vector5_buf;
+overflow_vector6 <= overflow_vector6_buf;
+overflow_vector7 <= overflow_vector7_buf;
+overflow_vector8 <= overflow_vector8_buf;
+overflow_vector9 <= overflow_vector9_buf;
+overflow_vector10 <= overflow_vector10_buf;
+overflow_vector11 <= overflow_vector11_buf;
+overflow_vector12 <= overflow_vector12_buf;
+overflow_vector13 <= overflow_vector13_buf;
+overflow_vector14 <= overflow_vector14_buf;
+overflow_vector15 <= overflow_vector15_buf;
 
 
 end bhv;
