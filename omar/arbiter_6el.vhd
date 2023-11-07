f92fac29ce45c8a87e9c9fe76986ba8d504a6ef2
BogdanBigMoney <149693159+BogdanBigMoney@users.noreply.github.com>
2023-11-07 22:39:26 +0100

    ADF
    BLYAT

--- omar/arbiter_6el.vhd
+++ omar/arbiter_6el.vhd
@@ -9,15 +9,18 @@ entity arbiter is
 
 -- inputs --data bus requests
 
-	req0,req1  : in  std_logic;
+	req0,req1,req2 : in  std_logic;
 	
 	busy : in  std_logic;
 	BUS_sync_a1,BUS_sync_a2 : OUT  std_logic;
 
 				
--- outputs   
+-- outputs  
+ 
     gnt0 : out std_logic;
-	gnt1 : out std_logic
+	gnt1 : out std_logic;
+		gnt2 : out std_logic
+
 	
   );
   
@@ -34,16 +37,19 @@ BEGIN
     IF (rst = '1') THEN
 		gnt0 <= '0';
 		gnt1 <= '0';
+		gnt2 <= '0';
     ELSIF (rising_edge(clk)) THEN
 		CASE state IS
 		WHEN Idle =>
 			gnt0 <= req0;
 			gnt1 <= req1 and not req0;
+			gnt2 <= req2 and not (req1 or req0);
 			state <= Granted;
 		WHEN Granted => 
 			IF busy = '1' THEN
 				gnt0 <= '0';
 				gnt1 <= '0';
+                                gnt2 <= '0';
 				state <= In_prog;
 			END IF;
 		WHEN In_prog => 
@@ -51,6 +57,7 @@ BEGIN
 				IF req0 = '1' OR req1 = '1' THEN 
 					gnt0 <= req0;
 					gnt1 <= req1 and not req0;
+						gnt2 <= req2 and not (req0 or req1);
 					state <= Granted;
 				ELSE 
 					state <= Idle;
